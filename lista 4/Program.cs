1bccc6f96e8d37902ae80cf6699af6d7c0f96f3d
Chr1stin4 <Christina.mei@aluno.ifsdp.edu.br>
2023-03-29 12:47:57 -0300

    Adicionar arquivos de projeto.

---
+++ lista 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercício 1
+
+            //essa afirmativa é falsa pois não tem como atribuir o float por int,mas int para float é possível
+            //exemplos:
+            //int a = 5;
+            //float b = a;
+            //Console.WriteLine(b); // resultado: 5.0
+
+            //float c = 7.5f;
+            //int d = (int)c;
+            //Console.WriteLine(d); // resultado: 7
+                                  //
+
+            //exercício 2
+            //Console.read() le apenas um caracter
+            //Console.readline() le mais de um caracter 
+
+            //exercício 3
+            // o valor tem que ser valido para o tipo de variavel que está sendo aplicado
+
+            //exercício 4           
+            //o objetivo do programa é verificar se o valor é par e maioe que 10
+            //se voce entrar com 8 sera "mensagem 2"
+            //se voce entrar com 27 sera "mensagem 2"
+            //se voce entrar com 28 sera "mensagem 1"
+
+            //exercício 5
+            //
+
+
+
+
+
+
+        }
+    }
+     
+}
+
+   
+
+
